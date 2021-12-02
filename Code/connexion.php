31086f06b036e58d53f940b58f0863c2d53133df
Annaël Moussa <annaelmoussa@gmail.com>
2021-12-02 18:52:25 +0100

    Ajout du fichier de connexion pour une connexion en local

--- Code/connexion.php
+++ Code/connexion.php
@@ -1,1 +1,12 @@
 <?php
+class Connexion{
+    protected static $bdd;
+    private static $dns='mysql:host=localhost;dbname=blog';
+    private static $user='root';
+    private static $password='';
+    public static function initConnexion(){
+        self::$bdd = new PDO(self::$dns,self::$user,self::$password);
+    }
+}
+
+?>
