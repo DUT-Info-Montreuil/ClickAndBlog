7c6dce8e6e1939dd6b51f1efc69297bba9c9a086
Louis BONNET <lbonnet@b110-19.iut.univ-paris8.fr>
2021-12-09 12:15:23 +0100

    ajour article bayern

--- Code/connexion.php
+++ Code/connexion.php
@@ -4,7 +4,7 @@ class Connexion{
     // private static $dns='mysql:host=localhost;dbname=blog';
     // private static $user='root';
     // private static $password='';
-        
+     
     public static function initConnexion(){
         self::$bdd = new PDO(self::$dns,self::$user,self::$password);
     }
