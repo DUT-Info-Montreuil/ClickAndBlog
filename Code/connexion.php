ec8a9e6eceacf1855a2504ecef24cb2225b4cb5c
Lotfi-Touil <lotfi.touil7@gmail.com>
2021-12-09 11:45:57 +0100

    correctif

--- Code/connexion.php
+++ Code/connexion.php
@@ -4,10 +4,6 @@ class Connexion{
     // private static $dns='mysql:host=localhost;dbname=blog';
     // private static $user='root';
     // private static $password='';
-        /***** IUT */
-        private static $dns = 'mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201660';
-        private static $user = 'dutinfopw201660';
-        private static $password  = 'munupeby';
         
     public static function initConnexion(){
         self::$bdd = new PDO(self::$dns,self::$user,self::$password);
