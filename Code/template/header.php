a8e00fc144f1bac5505fd6132dee323bfd44fee2
Annaël Moussa <annaelmoussa@gmail.com>
2021-12-14 21:31:46 +0100

    Ajout du framework TinyMCE dans la vue du form

--- Code/template/header.php
+++ Code/template/header.php
@@ -13,8 +13,12 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <!-- Jquery -->
     <script src="composants/Js/Jquery/jquery.js"></script>
+    <!-- TinyMCE -->
+    <script src="composants/Js/TinyMce/tinymce.min.js"></script>
     <!-- JS -->
     <script defer src="scripts.js"></script>
+    <!-- ScriptTinyMCE -->
+    <script src="composants/Js/Script/tiny.js"></script>
 </head>
 <body>
 <?php
