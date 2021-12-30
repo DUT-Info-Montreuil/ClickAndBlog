cbb0a3afc7e2c30835dd124976176711c19198e8
Annaël <annaelmoussa@gmail.com>
2021-12-30 15:23:47 +0100

    Correction footer affichage article

--- Code/template/footer.php
+++ Code/template/footer.php
@@ -1,7 +1,7 @@
 </section>
 </body>
-<footer>
-    <div>
+<footer class="footer">
+    <div class="content has-text-centered">
         <hr style="width:100%; margin:auto; color:black; background-color:#70a1ff; height:5px; opacity: 0.7;">
         <div class="has-text-centered has-text-black ">
             <a href="">Click & Blog</a>
