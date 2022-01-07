6fbe2a120ed84fba6ebee65e6525b57e0e03c7d9
Louis BONNET <lbonnet@a205-01.iut.univ-paris8.fr>
2022-01-07 11:53:37 +0100

    ajou export bd

--- Code/connexion.php
+++ Code/connexion.php
@@ -3,14 +3,14 @@ class Connexion{
     protected static $bdd;
     
     /***** annaelmoussa.me */
-    private static $dns = 'mysql:host=annaelmoussa.me;dbname=dzkq5840_Click&Blog';
-    private static $user = 'dzkq5840_lotfi_touil';
-    private static $password  = '5Q[=LO}uk=.f';
+    // private static $dns = 'mysql:host=annaelmoussa.me;dbname=dzkq5840_Click&Blog';
+    // private static $user = 'dzkq5840_lotfi_touil';
+    // private static $password  = '5Q[=LO}uk=.f';
 
       /***** IUT de Montreuil */
-    // private static $dns = 'mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=';
-    // private static $user = '';
-    // private static $password  = '';
+    private static $dns = 'mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=';
+    private static $user = '';
+    private static $password  = '';
 
     public static function initConnexion(){
         self::$bdd = new PDO(self::$dns,self::$user,self::$password);
