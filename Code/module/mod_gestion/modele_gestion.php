8ebac00a4f27e69430d864e8c791cd3377f9cfdc
Lotfi-Touil <lotfi.touil7@gmail.com>
2022-01-11 14:32:25 +0100

    changement de  nom, prenom et username (gestion de compte)

--- Code/module/mod_gestion/modele_gestion.php
+++ Code/module/mod_gestion/modele_gestion.php
@@ -7,6 +7,10 @@ class ModeleGestion extends Connexion
     {
         $selectPrep = self::$bdd->prepare('UPDATE user_connect SET nom = ? where id = ?');
         $selectPrep->execute(array($_POST['nom'], $_SESSION['id']));
+        $selectPrep = self::$bdd->prepare('UPDATE user_connect SET prenom = ? where id = ?');
+        $selectPrep->execute(array($_POST['prenom'], $_SESSION['id']));
+        $selectPrep = self::$bdd->prepare('UPDATE user_connect SET username = ? where id = ?');
+        $selectPrep->execute(array($_POST['username'], $_SESSION['id']));
         header('Location: index.php?module=mod_gestion&action=profil');
     }
 }
