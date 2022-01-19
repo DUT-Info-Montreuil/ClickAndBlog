2b2021ed42ac4ef28f6aac412e2776d4f6d08f8b
Annaël Moussa Adamou <annaelmoussa@gmail.com>
2022-01-19 16:11:47 +0100

    Ajout de la barre de recherche

--- Code/index.php
+++ Code/index.php
@@ -50,6 +50,7 @@ if(isset($_GET['module'])){
 $contenuTampon = $main->getControlleur()->getVue()->getAffichage();
 require_once 'template/header.php';
 ?>
+<section class="result"></section>
 <section class="container">
 <?php
 echo $contenuTampon;
